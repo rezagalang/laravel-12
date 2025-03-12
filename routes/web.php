ea7544eb4f14668ce308a2707f53eb184028f9e2
rezagalang <r3za.dev@gmail.com>
2025-03-12 17:40:44 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AdminController::class, 'index'])->name('dashboard');
